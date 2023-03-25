403f6099c6afe6cc931c3f3b1f6a289a7d84b04f
DrInnovatio <turing1723@gmail.com>
2023-03-25 21:38:48 +0900

    the second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Slide } from 'react-slideshow-image';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import logo from '../src/images/logo.png';
+import ad_1 from '../src/images/ad_1.jpeg'
+import 'react-slideshow-image/dist/styles.css'
+
+// import slide_1 from '../src/images/slide_1.jpg'
+
+
+const styles = {
+  main: {
+    width: "100%",
+    height: "800px"
+  },
+  inputs: {
+    flex: 1,
+    height: '800px',
+    paddingTop: '3rem',
+    flexDirection: 'column',
+    justifyContent: 'space-between', //space-around
+    marginLeft: '8rem',
+    borderRadius: '50px',
+    background: 'linear-gradient(145deg, #cacaca, #f0f0f0)',
+    boxShadow: 'inset 13px 13px 26px #d0d0d0, inset -13px -13px 26px #f0f0f0'
+    
+  },
+  inputText: {
+      padding: "10px"
+  },
+  a: {
+    backgroundColor: 'yellow'
+  },
+  
+
+};
+const spanStyle = {
+  padding: '20px',
+  background: '#efefef',
+  color: '#000000'
+}
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  width: '100%',
+  height: '1150px',
+  
+}
+const slideImages = [
+  {
+    url: `${ad_1}`,
+    caption: 'Slide 1'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Slide 3'
+  },
+];
+
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                <span style={spanStyle}>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+      </div>
+    )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" style={styles.main}>
+      <div>
+        <section>
+          <Slideshow/>
+        </section>
+        <section className="one-fourth" style={styles.a}>
+          <img src={logo} />
+        </section>
+        
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 1, width: '55ch' },
+          }}
+          noValidate
+          autoComplete="off"
+          style={ styles.inputs }
+          maxWidth="md"
         >
-          Learn React
-        </a>
-      </header>
+      <TextField id="outlined-basic" label="이름" variant="outlined" />
+      <TextField id="outlined-basic" label="핸드폰 번호" variant="outlined" />
+      <TextField id="outlined-basic" label="매장 상호명" variant="outlined" />
+      <TextField id="outlined-basic" label="매장 주소" variant="outlined" />
+      <TextField id="outlined-basic" label="인기매뉴" variant="outlined" />
+      <TextField id="outlined-basic" label="인기매뉴 가격" variant="outlined" />
+      
+      </Box>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 4, width: '55ch', marginTop: '6rem' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+      <div>
+       
+      <TextField
+          id="outlined-multiline-static"
+          label="간단한 매장 소개"
+          multiline              
+          rows={4}
+          defaultValue=""
+      />
+        </div>
+    </Box>
+      </div>
     </div>
   );
 }
