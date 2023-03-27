3a4b6be35b973f119f8d56b5778aeed09d808c5d
DrInnovatio <turing1723@gmail.com>
2023-03-27 09:44:20 +0900

    saparated slideshow component.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { Slide } from 'react-slideshow-image';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import logo from '../src/images/logo.png';
-import ad_1 from '../src/images/ad_1.jpeg'
+
 import 'react-slideshow-image/dist/styles.css'
+
+import Slideshow from './components/slideshow';
+
 
 // import slide_1 from '../src/images/slide_1.jpg'
 
@@ -35,51 +38,8 @@ const styles = {
   
 
 };
-const spanStyle = {
-  padding: '20px',
-  background: '#efefef',
-  color: '#000000'
-}
 
-const divStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundSize: 'cover',
-  width: '100%',
-  height: '1150px',
-  
-}
-const slideImages = [
-  {
-    url: `${ad_1}`,
-    caption: 'Slide 1'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
-    caption: 'Slide 2'
-  },
-  {
-    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-    caption: 'Slide 3'
-  },
-];
 
-const Slideshow = () => {
-    return (
-      <div className="slide-container">
-        <Slide>
-         {slideImages.map((slideImage, index)=> (
-            <div key={index}>
-              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-                <span style={spanStyle}>{slideImage.caption}</span>
-              </div>
-            </div>
-          ))} 
-        </Slide>
-      </div>
-    )
-}
 
 
 
---
+++ src/components/slideshow.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { Slide } from 'react-slideshow-image';
+import ad_1 from '../images/ad_1.jpeg';
+import 'react-slideshow-image/dist/styles.css';
+
+
+const spanStyle = {
+  padding: '20px',
+  background: '#efefef',
+  color: '#000000'
+}
+
+const divStyle = {
+  width: '50rem',
+  height: '50rem',
+  display: 'column',
+  flex:1,
+  justifContent: 'center',
+  alignItems: 'center',
+  overflow: 'hidden',
+  objectFit: 'cover',
+  position: 'relative',
+  
+}
+
+const slideImages = [
+  {
+    url: `${ad_1}`,
+    caption: 'Slide 1'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+    caption: 'Slide 2'
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    caption: 'Slide 3'
+  },
+];
+
+function Slideshow() {
+  return (
+    <div className="slide-container">
+        <Slide>
+         {slideImages.map((slideImage, index)=> (
+            <div key={index}>
+              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
+                <span style={spanStyle}>{slideImage.caption}</span>
+              </div>
+            </div>
+          ))} 
+        </Slide>
+    </div>
+  );
+};
+
+export default Slideshow;
