2d03e29ab9ee599e57897584fa70648f0a0c74f9
DrInnovatio <turing1723@gmail.com>
2023-03-31 00:15:58 +0900

    changed the form.

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,8 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import RowRadioButtonsGroup from '../components/radioButton';
+import { db } from '../../src/firebase';
 
 
 const styles = {
@@ -22,30 +24,41 @@ const styles = {
 };
 
 export default function BasicTextFields() {
+
+  const [name, setName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [restaurantName, setRestaurantName] = useState("");
+  const [address, setAddress] = useState("");
+  const [price, setPrice] = useState("");
+
+
   return (
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '60ch' },
-      }}
-      noValidate
-      autoComplete="off"
-      style={styles.inputs}
-    >
-      <TextField style={styles.a} id="outlined-basic" label="이름" variant="outlined" />
-      <TextField style={styles.a} id="outlined-basic" label="핸드폰 번호" variant="outlined" />
-      <TextField style={styles.a} id="outlined-basic" label="매장 상호명" variant="outlined" />
-      <TextField style={styles.a} id="outlined-basic" label="매장 주소" variant="outlined" />
-      <TextField style={styles.a} id="outlined-basic" label="인기매뉴" variant="outlined" />
-      <TextField style={styles.a} id="outlined-basic" label="인기매뉴 가격" variant="outlined" />
-      <TextField
-        id="outlined-multiline-static"
-        label="간단한 매장 소개"
-        multiline              
-        rows={4}
-        defaultValue=""
-        style={styles.a}
-      />
-    </Box>
+    <form className="form">
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '60ch' },
+        }}
+        noValidate
+        autoComplete="off"
+        style={styles.inputs}
+      >
+        <TextField style={styles.a} id="name" label="이름" variant="outlined" />
+        <TextField style={styles.a} id="mobil" label="핸드폰 번호" variant="outlined" />
+        <TextField style={styles.a} id="restaurantName" label="매장 상호명" variant="outlined" />
+        <TextField style={styles.a} id="address" label="매장 주소" variant="outlined" />
+        <TextField style={styles.a} id="menu" label="인기매뉴" variant="outlined" />
+        <TextField style={styles.a} id="price" label="인기매뉴 가격" variant="outlined" />
+        <TextField
+          id="outlined-multiline-static"
+          label="간단한 매장 소개"
+          multiline              
+          rows={4}
+          defaultValue=""
+          style={styles.a}
+        />
+        <RowRadioButtonsGroup/>
+        </Box>
+    </form>
   );
 }
