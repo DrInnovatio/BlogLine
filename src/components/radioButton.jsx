2ba24b859dbd32953c822c4e14ba68695d02ae43
DrInnovatio <turing1723@gmail.com>
2023-03-31 00:15:44 +0900

    added Radio button.

---
+++ src/components/radioButton.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+export default function RowRadioButtonsGroup() {
+  return (
+    <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">나는 온라인 광고를 해본 경험이 ..</FormLabel>
+      <RadioGroup
+        row
+        aria-labelledby="demo-row-radio-buttons-group-label"
+        name="row-radio-buttons-group"
+      >
+        <FormControlLabel value="yes" control={<Radio />} label="있다" />
+        <FormControlLabel value="no" control={<Radio />} label="없다" />  
+      </RadioGroup>
+    </FormControl>
+  );
+}
