6c767bda4fde9844341fe267906b9ae25b603e22
DrInnovatio <turing1723@gmail.com>
2023-04-03 23:00:07 +0900

    fixed radio button and check box.

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,16 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import RowRadioButtonsGroup from '../components/radioButton';
-import { db } from '../../src/firebase';
-import { getDocs, collection, addDoc } from 'firebase/firestore';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import SimpleAccordion from './accordion';
+import Checkbox from '@mui/material/Checkbox';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../firebase';
 
 
 const styles = {
   inputs: {
     flex: 1,
     width: '55rem',
-    height: '40rem',
+    height: '60rem',
     flexDirection: 'column',
     justifyContent: 'space-between', //space-around
     margin: '2rem auto',
@@ -24,6 +32,8 @@ const styles = {
   }
 };
 
+
+
 export default function BasicTextFields() {
 
   const [name, setName] = useState("");
@@ -34,9 +44,56 @@ export default function BasicTextFields() {
   const [menu, setMenu] = useState("");
   const [price, setPrice] = useState(0);
   const [introduce, setIntroduce] = useState("");
- 
-  
+  const [isExperienced, setIsExperienced] = useState(true);
+  const [isAgreed, setIsAgreed] = useState(false);
 
+  const ownersCollectionRef = collection(db, "owners")
+  
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(isExperienced); // Replace with code to send data to backend
+  // };
+
+  const handleIsAgreedChange = (event) => {
+    setIsAgreed(event.target.checked);
+  };
+
+
+  const handleAnswerChange = (event) => {
+    setIsExperienced(event.target.value);
+  };
+
+  const submit = async (event) => {
+    
+    try {
+      await addDoc(ownersCollectionRef, {
+        name: name,
+        mobile: mobile,
+        email: email,
+        restaurantName: restaurantName,
+        address: address,
+        menu: menu,
+        price: price,
+        introduce: introduce,
+        isAgreed: isAgreed,
+        isExperienced: isExperienced
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    event.preventDefault();
+    if (isAgreed) {
+      // Replace with code to log the user in
+      console.log("Logged in");
+      event.preventDefault();
+      // Replace with code to send data to backend
+      window.location.reload(); // Refresh the page
+    } else {
+      alert("Please confirm that you want to login.");     
+    }
+  };
+
+  
   return (
     <form className="form">
       <Box
@@ -48,7 +105,10 @@ export default function BasicTextFields() {
         autoComplete="off"
         style={styles.inputs}
       >
-        <TextField style={ styles.a } id="name" label="이름" variant="outlined" onChange={(e) => setName(e.target.value)} />
+        
+        
+        {/* Basic form */}
+        <TextField style={styles.a } id="name" label="이름" variant="outlined" onChange={(e) => setName(e.target.value)} />
         <TextField style={styles.a} id="mobil" label="핸드폰 번호" variant="outlined" onChange={(e) => setMobile(e.target.value)}/>
         <TextField style={styles.a} id="email" label="이메일" variant="outlined" onChange={(e) => setEmail(e.target.value)}/>
         <TextField style={styles.a} id="restaurantName" label="매장 상호명" variant="outlined" onChange={(e) => setRestaurantName(e.target.value)}/>
@@ -64,8 +124,53 @@ export default function BasicTextFields() {
           style={ styles.a }
           onChange={(e) => setIntroduce(e.target.value)}
         />
-        <RowRadioButtonsGroup/>
-        </Box>
-    </form>
+        {/* Basic form */}
+
+        {/* Experience of online Ads - Radio box*/}
+        <FormControl >
+          <FormLabel id="demo-row-radio-buttons-group-label">나는 온라인 광고를 해본 경험이 ..</FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-row-radio-buttons-group-label"
+            name="row-radio-buttons-group"
+          >
+            <FormControlLabel value="yes" control={<Radio />} label="있다" checked={isExperienced === 'yes'} onChange={handleAnswerChange}/>
+            <FormControlLabel value="no" control={ <Radio /> } label="없다" checked={isExperienced === 'no'} onChange={handleAnswerChange}/>   
+          </RadioGroup>
+        </FormControl>
+
+       
+
+        {/* Experience of online Ads - Radio box*/}
+        
+        {/* Agreement */}
+          <SimpleAccordion />
+        {/* Agreement */ }
+        
+        {/* Agreement - Check Box*/ }
+        <FormGroup onSubmit={submit}>
+          <FormControlLabel control={<Checkbox defaultChecked />} label="
+              개인정보 수집 동의서를 읽었으며 동의 합니다."
+              checked={ isAgreed }
+              onChange={ handleIsAgreedChange } 
+          />
+
+          <Button 
+              
+              onClick={ submit }
+              variant="contained"
+              style={ { width: '15rem', marginTop: '5rem' } }>
+            제출
+          </Button>
+          
+        </FormGroup>
+        {/* Agreement - Check Box*/ }
+        
+
+      
+      </Box>
+      </form>
+    
+    
   );
 }
